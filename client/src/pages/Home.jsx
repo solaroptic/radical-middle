f416bb4420fc7c1c4b8c7bede607615288d4921e
John Fabiszewski <kumolanding@gmail.com>
2024-05-24 09:31:59 -0500

    array check

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
         avoiding the extremes of both the right and the left.
       </h3>
       <div className={styles.posts}>
-        {posts?.length > 0 ? (
+        {Array.isArray(posts) ? (
           posts?.map((post) => (
             <div
               className={`${styles.post} ${smallScreen ? styles.small : ""} `}
