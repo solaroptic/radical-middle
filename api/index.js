ced6da318257474b41e57291967965522a0dd3b8
John Fabiszewski <kumolanding@gmail.com>
2024-05-25 10:20:29 -0500

    route switch

--- api/index.js
+++ api/index.js
@@ -8,6 +8,18 @@ import usersRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN, // Replace with your React app's origin
+    credentials: true, // Include if your API requires cookies for authentication
+  })
+);
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
@@ -18,19 +30,6 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.get("/", (req, res) => res.send("server is ready"));
 }
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_ORIGIN, // Replace with your React app's origin
-    credentials: true, // Include if your API requires cookies for authentication
-  })
-);
-app.use(express.json());
-app.use(cookieParser());
-
-app.use("/api/posts", postsRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 10000;
 app.listen(port, () => {
